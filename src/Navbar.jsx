54deaba74b7c49dc83eaf082ad345d230c14e23d
tahsinkarim <tahsinnkarim@gmail.com>
2022-06-26 23:44:34 +0600

    Sidebar Styling

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,23 +2,26 @@ import { Component } from "react";
 
 class Navbar extends Component {
   render() {
+    let Links = [
+      { name: "Services", link: "/#services" },
+      { name: "Pricing", link: "/#pricing" },
+      { name: "Our Story", link: "/#our-story" },
+    ];
     return (
-      <header className='Navbar bg-gradient-to-b from-dark-gray to-light-gray text-white h-20 flex items-center'>
-        <nav className='flex flex-row justify-between items-center mx-auto w-[1440px]'>
+      <header className='Navbar shadow-md bg-gradient-to-b from-dark-gray to-light-gray text-white py-5'>
+        <nav className='md:flex items-center justify-between'>
           <div className='logo'>
-            <h3 className='font-bold mx-16 font-heading'>Code-Ed</h3>
+            <h3 className='font-bold mx-16 font-heading'>
+              <a href='/#home'>Code-Ed</a>
+            </h3>
           </div>
-          <div className='Menu mr-16'>
-            <ul className='flex justify-between items-center gap-16 text-sm'>
-              <li>
-                <a href='#'>Services</a>
-              </li>
-              <li>
-                <a href='#'>Pricing</a>
-              </li>
-              <li>
-                <a href='#'>Our Story</a>
-              </li>
+          <div className='Menu mr-16 md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-gradient-to-b from-light-gray to-dark-gray md:z-auto z-[-1]'>
+            <ul className='gap-16 text-sm md:flex md:items-center'>
+              {Links.map((link) => (
+                <li key={link.name} className='md:my-0 my-7'>
+                  <a href={link.link}>{link.name}</a>
+                </li>
+              ))}
               <li>
                 <button className='font-bold border-2 px-8 py-2 text-xs'>
                   TRY FOR FREE
